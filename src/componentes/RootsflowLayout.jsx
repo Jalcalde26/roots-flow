adb0b7dbc226a967d4fac4f5664f861a05ea5fbe
Tu Nombre o Usuario de GitHub <tu_email_de_github@ejemplo.com>
2026-09-16 21:36:46 +0200

    update:

--- src/componentes/RootsflowLayout.jsx
+++ src/componentes/RootsflowLayout.jsx
@@ -1,69 +1,85 @@
 import 'family-chart/styles/family-chart.css';
 import '../index.css';
-import { useState, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 function RootsflowLayout ({ children, onAsideTransitionEnd }) {
 
     const [isColapsed, setIsColapsed] = useState(false);
     const [showLeftArrow, setShowLeftArrow] = useState(false);
     const [isDragging, setIsDragging] = useState(false);
-    const [asideWidth, setAsideWidth] = useState("w-[30vw]");
-    const asideRef = useRef(null);
+    const [asideWidth, setAsideWidth] = useState("30vw");
+    const asideWidthRef = useRef(asideWidth);
 
-// COMENZAR A TRABAJAR AQUI
 
-/* 
-useEffect(() => {
-    if (!isDragging) return;
+    useEffect(() => {
+        if (!isDragging) {
+            document.body.style.userSelect = "";
+            return; 
+        } 
+        
+        document.body.style.userSelect = "none";
+        
 
-    function handleMouseMove(evento) {
-        // tu lógica de cálculo de nuevoWidth aquí
-    }
+        function handleMouseMove(e) {
+            const newWidth = document.documentElement.clientWidth - e.clientX;
+            asideWidthRef.current = newWidth;
+            console.log(newWidth);
 
-    function handleMouseUp(evento) {
-        setIsDragging(false);
-        onAsideTransitionEnd();
-    }
+            if (newWidth >= document.documentElement.clientWidth / 2) {
+                setAsideWidth(`50vw`);
+                return;
+            }
 
-    document.addEventListener("mousemove", handleMouseMove);
-    document.addEventListener("mouseup", handleMouseUp);
+            if (newWidth <= 24) {
+                setAsideWidth(`1.5rem`);//1.5rem son 24px         
+                return;
+            }
 
-    return () => {
-        document.removeEventListener("mousemove", handleMouseMove);
-        document.removeEventListener("mouseup", handleMouseUp);
-    };
-}, [isDragging]);
-*/
+            setAsideWidth(`${newWidth}px`);               
+        };
+
+        function handleMouseUp() {
+
+            setIsDragging(false);
+            if (asideWidthRef.current <= 24) {
+                setIsColapsed(true);
+                setShowLeftArrow(true);
+            }
+            if (asideWidthRef.current >= 24) {
+                setIsColapsed(false);
+                setShowLeftArrow(false);
+            }
+            onAsideTransitionEnd();
+        };
+
+        document.addEventListener("mousemove", handleMouseMove);
+        document.addEventListener("mouseup", handleMouseUp);
+
+        return () => {
+            document.removeEventListener("mousemove", handleMouseMove);
+            document.removeEventListener("mouseup", handleMouseUp);
+        };
+
+    }, [isDragging]);
+
+
 
     function handleMouseDown (e) {
+        e.preventDefault();
         setIsDragging(true);
-        console.log("click");
-    };
-
-    function handleMouseUp (e) {
-        const positionMouse = e.clientX;
-        const viewportWidth = window.innerWidth;
-
-        if (isDragging === true) {
-            setIsDragging(false);
-            setAsideWidth(`w-[${viewportWidth - positionMouse}px]`);
-            onAsideTransitionEnd();            
-        }
-
-        return;
     };
 
     const handleToggle = () => {
         // Usamos una variable local para lidiar con la asincronia del estado y no leer el estado desactualizado
         const newIsColapsed = !isColapsed;
         setIsColapsed(newIsColapsed);
-        setAsideWidth(newIsColapsed ? "w-[1.5rem]" : "w-[30vw]");
+        setAsideWidth(newIsColapsed ? "1.5rem" : "30vw");
     };
 
     const handleTransitionEnd = (e) => {
         if (e.propertyName !== "width") return;
         setShowLeftArrow(isColapsed);
-        onAsideTransitionEnd();
+        if (!isDragging) onAsideTransitionEnd();
     };
 
 
@@ -72,7 +88,8 @@ useEffect(() => {
             <div className="relative">
                 {children}
             </div>
-            <aside className={`relative bg-[#212121] shadow-[-14px_0_8px_-6px_rgba(0,0,0,0.3)] transition-all duration-600 ${asideWidth}`}
+            <aside className={`relative bg-[#212121] shadow-[-14px_0_8px_-6px_rgba(0,0,0,0.3)] transition-all ${isDragging ? "duration-0" : "duration-700"}`}
+                    style={{ width: `${asideWidth}`}}
                     onTransitionEnd={handleTransitionEnd}>
                 <button
                     className={`absolute top-1/2 left-0 -translate-y-1/2 -translate-x-1/2 text-xl rounded-full flex justify-center items-center w-9 h-9 bg-gray-600 shadow-[-14px_0_8px_-6px_rgba(0,0,0,0.3)] cursor-pointer z-10 transition-transform duration-400 hover:scale-120 will-change-transform`}
@@ -82,9 +99,8 @@ useEffect(() => {
                 </button>
                 {/* div para detectar borde izq del aside de forma consistente*/}
                 <div 
-                    ref={asideRef}
                     onMouseDown={handleMouseDown}  
-                    className={`absolute w-2 h-full -translate-x-1/2 border border-white z-5`}> 
+                    className={`absolute w-4 h-full -translate-x-1/2  border-white cursor-col-resize z-5`}> 
                 </div>
             </aside>
         </div>
